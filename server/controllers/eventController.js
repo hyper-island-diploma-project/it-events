75fae77d2e0adeed689acd94ee05918ae493900d
Kseniia Tikhonova <tikhonovakb@gmail.com>
2024-06-14 14:30:17 +0200

    feat: add routing and endpoints for user

---
+++ server/controllers/eventController.js
@@ -0,0 +1,9 @@
+class eventController {
+  async createEvent(req, res) {}
+
+  async getAllEvents(req, res) {}
+
+  async getOneEvent(req, res) {}
+}
+
+module.exports = new eventController();
--- server/index.js
+++ server/index.js
@@ -2,15 +2,18 @@ require("dotenv").config();
 const express = require("express");
 const sequelize = require("./db");
 const models = require("./models/models");
-const cors = require('cors');
+const cors = require("cors");
+const router = require("./routes/index");
+const errorHandler = require("./middleware/errorHandlerMiddlewere");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/api", router);
 
-app.get()
+app.use(errorHandler);
 
 const start = async () => {
   try {
