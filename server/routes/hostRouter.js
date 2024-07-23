748e2600d7cc4bfaf75785f67f18747d1d7c8994
Kseniia Tikhonova <tikhonovakb@gmail.com>
2024-07-23 19:44:47 +0500

    feat: create checkRole and auth middleware

--- server/routes/hostRouter.js
+++ server/routes/hostRouter.js
@@ -1,8 +1,9 @@
 const Router = require("express");
 const router = new Router();
 const hostController = require("../controllers/hostController");
+const checkRole = require("../middleware/checkRoleMiddleware");
 
-router.post("/", hostController.create);
+router.post("/", checkRole('ADMIN'), hostController.create);
 router.get("/", hostController.getAll);
 router.delete("/:id", hostController.delete);
 
