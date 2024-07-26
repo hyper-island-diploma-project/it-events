f277e27eac08041b0e1b68138aa0d67887d278f2
Kseniia Tikhonova <tikhonovakb@gmail.com>
2024-07-26 21:19:43 +0500

    feat: create user subscription controller and router

---
+++ server/controllers/userSubscriptionController.js
@@ -0,0 +1,55 @@
+const ApiError = require("../error/ApiError.js");
+const { UserSubscription, User, Event } = require("../models/models.js");
+
+class UserSubscriptionController {
+  async create(req, res, next) {
+    try {
+      const { userId, eventId } = req.body;
+
+      const userSubscription = await UserSubscription.create({
+        userId,
+        eventId,
+      });
+      return res.json(userSubscription);
+    } catch (error) {
+      console.error(error);
+      return next(ApiError.internal("Something went wrong"));
+    }
+  }
+
+  async getAllByUserId(req, res) {
+    const { id } = req.params;
+
+    try {
+      const subscriptions = await UserSubscription.findAll({
+        where: { userId: id },
+        include: [
+          {
+            model: Event,
+          },
+        ],
+      });
+      return res.json(subscriptions);
+    } catch (error) {
+      console.error(error);
+      return next(ApiError.internal("Something went wrong"));
+    }
+  }
+
+  async delete(req, res, next) {
+    const { id } = req.params;
+    try {
+      const subscription = await UserSubscription.findOne({ where: { id } });
+      if (!subscription) {
+        return next(ApiError.badRequest("Subscription not found"));
+      }
+      await subscription.destroy();
+
+      return res.json({ message: "Subscription deleted successfully" });
+    } catch (error) {
+      return next(ApiError.internal("Something went wrong"));
+    }
+  }
+}
+
+module.exports = new UserSubscriptionController();
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,11 @@ const router = new Router();
 const eventRouter = require("./eventRouter");
 const userRouter = require("./userRouter");
 const hostRouter = require("./hostRouter");
+const userSubscriptionRouter = require("./userSubscriptionRouter");
 
 router.use("/user", userRouter);
 router.use("/event", eventRouter);
 router.use("/host", hostRouter);
+router.use("/subscription", userSubscriptionRouter);
 
 module.exports = router;
